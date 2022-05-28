3e52331769d8bb4776d9728d29eb760b468fa4bb
ezazsabit <sabit20.kuet@gmail.com>
2022-05-28 12:18:04 +0600

    handle Error

--- src/Purchasepage/Purchace.js
+++ src/Purchasepage/Purchace.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
 import auth from '../firebase.init';
@@ -15,18 +16,45 @@ const Purchace = () => {
             .then((data) => setData(data));
     }, [id, data]);
     // ---------------------------------------------user info from log-in
- 
-        const [user, loading, error] = useAuthState(auth);
-        if (loading) {
-            return (
-              <div>
-                <p>Initialising User...</p>
-              </div>
-            );
-          }
-        //   console.log(user)
-        // .............................................
 
+    const [user, loading] = useAuthState(auth);
+    if (loading) {
+        return (
+            <div>
+                <p>Initialising User...</p>
+            </div>
+        );
+    }
+    //   console.log(user)
+    // .............................................handle quantity
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [error,setError] =  useState(false)
+   
+    
+    const handleQuantity=(event)=>{
+        // console.log(event.target.value)
+        let a=parseInt(event.target.value);
+        console.log(a)
+        console.log(a>data.AvailableQuantity)
+      
+        if(a<data.MinimumOrderQuantity || a>data.AvailableQuantity)
+        {
+               setError(true)
+        }
+        else setError(false)
+        
+    
+    }
+    let errorHandle;
+    if(error){
+        errorHandle=
+        <div>
+         <p className='text-lg text-red-700'>You have given wrong quantity.Please try again!!</p>
+        </div>
+    }
+ 
+   
+    // console.log(typeof(data.AvailableQuantity))
     return (
         <div>
             <p className='mt-5 text-6xl text-secondary'>Purchase from here!!</p>
@@ -46,6 +74,32 @@ const Purchace = () => {
                     </div>
                 </div>
             </div>
+            <p className='mb-5 text-6xl text-secondary'>Purchase Now!!</p>
+            <Form cl>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter address" />
+                    
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Phone No</Form.Label>
+                    <Form.Control type="tel" placeholder="Phone no." />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Quantity for purchase</Form.Label>
+                    {/* <Form.Control value={data.MinimumOrderQuantity} onBlur={handleQuantity()} type="number" placeholder="Quantity" /> */}
+                    <input defaultValue={data.MinimumOrderQuantity} onBlur={handleQuantity}  type="number" id="fname" name="fname" />
+                </Form.Group>
+               
+                {errorHandle}
+                
+
+               
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
         </div>
     );
 };
